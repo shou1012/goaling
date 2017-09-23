ee14574299d3b531481b78d585644576add5dffe
shohan ki <sho1012.seikyo@gmail.com>
2017-09-23 18:59:04 +0900

    ログイン・新規登録機能作成

--- Gemfile
+++ Gemfile
@@ -3,5 +3,15 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'sinatra-contrib'
-gem 'sinatra-activerecord'
 gem 'rake'
+gem 'sinatra-activerecord'
+
+gem 'bcrypt', '~> 3.1.7'
+
+group :development do
+gem 'sqlite3'
+end
+
+group :production do
+gem 'pg'
+end
--- app.rb
+++ app.rb
@@ -1,3 +1,57 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+require 'sinatra/activerecord'
+require './models/users'
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user])
+  end
+end
+
+enable :sessions
+
+get '/' do
+  erb :index
+end
+
+get '/signin' do
+  erb :sign_in
+end
+
+get '/signup' do
+  erb :sign_up
+end
+
+post '/signin' do
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user] = user.id
+  end
+  redirect '/'
+end
+
+post '/signup' do
+  user = User.create(
+    name: params[:name],
+    email: params[:email],
+    password: params[:password],
+    password_confirmation: params[:password_confirmation]
+  )
+  if user.persisted?
+    session[:user] = user.id
+  end
+  redirect '/'
+end
+
+get '/signout' do
+  session[:user] = nil
+  redirect '/'
+end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
